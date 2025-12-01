6b6c90deaa6859c5ec10a3441719bb925c430f0f
OMOLO PASIKALI <pasteccoltd@gmail.com>
2025-12-01 10:38:21 +0000

    update on parent's dashboard

--- src/dashboards/parent/ParentDashboard.jsx
+++ src/dashboards/parent/ParentDashboard.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import Sidebar from "./sidebar.jsx";
+import Header from "./header.jsx";
 
 function ParentDashboard() {
     return(
-        <div className="w-full h-auto p-8 bg-gray-100">
+        <div className="w-full h-screen p-8 bg-gray-600 flex">
             <Sidebar />
+            <div className="w-80/100 h-10/100 bg-green-400">
+                <Header />
+            </div>
         </div>
     );
 }
