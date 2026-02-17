74550512dac5c62facaafd4b06c0e6c5fb6fca10
OMOLO PASIKALI <pasteccoltd@gmail.com>
2026-02-17 12:44:05 +0000

    Modification on the admin dashboard

--- src/dashboards/admin/AdminCards.jsx
+++ src/dashboards/admin/AdminCards.jsx
@@ -6,36 +6,36 @@ function AdminCards (){
     return(
             <div className="admin-container">
                 <h1>Dashboard</h1>
-                <h2>Welcome Back!Here's your overview.</h2>
-                <div style={{display:"flex",gap:"30px"}}>
-                    <div className="admin-view">
-                        <div style={{display:"flex",alignItems:"center",justifyContent:"center",gap:"30px"}}>
-                            <h1>Total Students</h1>
+                <h2>Welcome Back! Here's your overview.</h2>
+                <div className="admin-dashboard-cards">
+                    <div className="admin-card flex flex-col justify-center items-center">
+                        <div className="flex flex-col items-center gap-2">
                             <i class="fa-solid fa-users" id="admin-icon-student"></i>
+                            <h1>Total Students</h1>
                         </div>
                         <p>1,234</p>
                     </div>
 
-                    <div className="admin-view">
-                        <div style={{display:"flex",alignItems:"center",justifyContent:"center",gap:"30px"}}>
-                            <h1>Total Teachers</h1>
+                    <div className="admin-card flex flex-col justify-center items-center">
+                        <div className="flex flex-col items-center gap-2">
                             <i class="fas fa-graduation-cap" id="admin-icons-teachers"></i>
+                            <h1>Total Teachers</h1>
                         </div>
                         <p>80</p>
                     </div>
 
-                    <div className="admin-view">
-                        <div style={{display:"flex",alignItems:"center",justifyContent:"center",gap:"30px"}}>
-                            <h1>Classes Today</h1>
+                    <div className="admin-card flex flex-col justify-center items-center">
+                        <div className="flex flex-col items-center gap-2">
                             <i class="fa-solid fa-book-open" id="admin-icons-classes"></i>
+                            <h1>Classes Today</h1>
                         </div>
                         <p>52</p>
                     </div>
 
-                    <div className="admin-view">
-                        <div style={{display:"flex",alignItems:"center",justifyContent:"center",gap:"30px"}}>
-                            <h1>Attendance Rate</h1>
+                    <div className="admin-card flex flex-col justify-center items-center">
+                        <div className="flex flex-col items-center gap-2">
                             <i class="fa-solid fa-users" id="admin-icons-rate"></i>
+                            <h1>Attendance Rate</h1>
                         </div>
                         <p>95.4%</p>
                     </div>
