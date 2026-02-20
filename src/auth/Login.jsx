371ace2ae9a9ce5ee5e966b03bb5ca6f82746b21
OMOLO PASIKALI <pasteccoltd@gmail.com>
2026-02-20 13:39:29 +0000

    on Login Form handles and states

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,13 +1,64 @@
 import { Link } from "react-router-dom";
 import React from 'react'
+import { useState, useEffect} from "react";
 import kyuLogo from '/src/assets/Logo.png'
 import './Login.css'
 
 
 function LoginForm(){
+    const [LoginData, setLoginData]= useState({
+        username:'',
+        password:''
+    })
+
+    // const [Loading, setLoading] = useState(false);
+    // const [LoadingMessage, setLoadingMessage] = useState('');
+
+    const handleChange = (e) => {
+         console.log(LoginData);
+         const { name, value } = e.target;
+
+        setLoginData(prev => ({
+        ...prev,
+        [name]: value
+        }));
+  };
+
+
+   const handleSubmit = async (e) => {
+        const [Message, setMessage] = useState("");
+        e.preventDefault();
+        setLoading(true);
+        setMessage("");
+
+        try {
+        await axios.post(
+            "http://localhost:5000/api/students",
+            LoginData
+        );
+
+        setMessage("Student registered successfully ✅");
+        console.log(Message)
+
+        setLoginData({
+            name: "",
+            email: "",
+            age: ""
+        });
+
+        } catch (error) {
+        setMessage("Error submitting form ❌");
+        console.log(Message)
+        }
+
+        setLoading(false);
+    };
+
+    
+
     return (
         <div className='loginForm'>
-            <form action="post" className='form-content'>
+            <form className='form-content ' onSubmit={handleSubmit}>
                 <div className="Kyu">
                     <img src={kyuLogo} alt="" className="KyuLogo" />
                 </div>
@@ -17,20 +68,38 @@ function LoginForm(){
                     <div className="uesr-name">
                         <label htmlFor="username">Username</label>
                         <div className="login-user-wrapper">
-                            <i class="fa-solid fa-user"></i>
-                            <input type="email" id='username' required placeholder='Username'/>
+                            <i className="fa-solid fa-user"></i>
+
+                            <input type="email" 
+                            id='username' 
+                            name="username"
+                            value={LoginData.username}
+                            onChange={handleChange}
+                            required 
+                            placeholder='Username'
+                           
+                            />
                         </div>
                     </div>
 
                     <div className="uesr-name">
                         <label htmlFor="password">Password</label>
                         <div className="login-user-wrapper">
-                            <i class="fa-solid fa-lock"></i>
-                            <input type="password" id='password'required placeholder='Password'/>
+                            <i className="fa-solid fa-lock"></i>
+
+                            <input type="password" 
+                            name="password"
+                            value={LoginData.password}
+                            onChange={handleChange}
+                            id='password'
+                            required 
+                            placeholder='Password'
+                            />
+
                         </div>
                     </div>
                     
-                    <button className="login-btn">Login</button>
+                    <button type="submit" className="login-btn">Login</button>
                     <p>Forgot password? <Link to='/RequestToken'  className="addressed" >Click here</Link> to Reset</p>
                 </div>
             </form>
