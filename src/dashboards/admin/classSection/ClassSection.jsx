3dfdc7e2ebf42da37461202f674cd52755d05991
OMOLO PASIKALI <pasteccoltd@gmail.com>
2026-03-09 19:14:48 +0000

    Added class section in the admin dashboard

--- src/dashboards/admin/classSection/ClassSection.jsx
+++ src/dashboards/admin/classSection/ClassSection.jsx
@@ -3,9 +3,9 @@ import Header from "/src/Header.jsx";
 // import SideBar from "./SideBar";
 // import AdminCards from "/src/dashboards/admin/adminCards/AdminCards.jsx";
 import ResponsiveHeader from "/src/ResponsiveHeader.jsx";
-import Body from "/src/dashboards/admin/Body.jsx";
 import '/src/dashboards/admin/Card.css';
 import AdminSidebar from "/src/dashboards/admin/AdminSidebar.jsx";
+import ClassBody from "/src/dashboards/admin/classSection/ClassBody.jsx";
 
 
 
@@ -25,7 +25,7 @@ function ClassSection (){
                     {/* <AdminCards/> */}
 
                     <div>
-                      {/* <Body/> */}
+                      <ClassBody/>
                     </div>
                 </div>
                 
