8b3a73f9661ce30c857c42736146125489dbad24
OMOLO PASIKALI <pasteccoltd@gmail.com>
2026-03-09 21:54:24 +0000

    update state handler for role, and cards on admin dashboard

--- src/dashboards/admin/AdminSidebar.jsx
+++ src/dashboards/admin/AdminSidebar.jsx
@@ -6,6 +6,17 @@ import AdminNavigation from "/src/dashboards/admin/adminNavigate/AdminNavigation
 
 function AdminSidebar(){
     const [role, setRole] = useState("User");
+
+    const handleRoleChange = async () => {
+        try{
+            const response = await fetch("/api/user/role");
+            setRole(response.data);
+            } catch (error) {
+        console.error("Error fetching user role:", error);
+    }
+        }
+    
+
     return(
         <SideBar role={role} navigation={<AdminNavigation />} />
     );
